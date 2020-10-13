1e52b910734cec43895cb49158f432ec48a123bf
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2020-10-12 19:47:12 -0600

    added more pages

--- app.py
+++ app.py
@@ -1,27 +1,44 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from MaplewoodScraper import MaplewoodScraper
 
-with open("loginInfo.txt") as f:
-        username, password = [line.strip() for line in f.readlines()]
-scrape = MaplewoodScraper(
-    username=username,
-    password=password
-)
-scrape.start(notify=False)
+scrape = None
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # return render_template("index.html")
+    return redirect("/signin")
+
+
+@app.route("/loading", methods=["POST"])
+def loading():
+    username = request.form["inputUsername"]
+    password = request.form["inputPassword"]
+    global scrape 
+    scrape = MaplewoodScraper(
+        username,
+        password
+    )
+    return render_template("loading.html")
+
+@app.route("/signin")
+def signin():
+    return render_template("signin.html")
+
+@app.route("/marks")
+def marks():
+    scrape.start(notify=False)
     return render_template("index.html", courses=scrape.courses, aliases=scrape.aliases)
+
 
 if __name__ == "__main__":
     app.run()
 
 
 # TODO: highlight percentage/row when a mark is below a certain perctentage
-# TODO: login page
+# TODO: signin page
 # TODO: show num assignments that are 100, mediocre and bad
 # TODO: show course average
 # TODO: deploy to heroku
+# TODO: verify login by making a new method in class
+# TODO: loading page
