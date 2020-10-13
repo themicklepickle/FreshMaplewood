a1cdb3b71df2e61507d6936e13a421374224c9e0
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2020-10-12 23:47:33 -0600

    added user authentication

--- app.py
+++ app.py
@@ -1,47 +1,43 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, session
 from MaplewoodScraper import MaplewoodScraper
 import requests
 
-username = ""
-password = ""
-error = False
-
 app = Flask(__name__)
+app.secret_key = "asdfkjh2i3u987ufiusjfdkjqwopr!@#$!@#%$#%@!#$!@#$%@!"
+
+session["error"] = False
+
 
 @app.route("/")
 def index():
     return redirect("/signin")
 
+
 @app.route("/loading", methods=["POST"])
 def loading():
-    global username
-    global password
-    username = request.form["inputUsername"]
-    password = request.form["inputPassword"]
+    session["username"] = request.form["inputUsername"]
+    session["password"] = request.form["inputPassword"]
     return render_template("loading.html")
+
 
 @app.route("/signin")
 def signin():
-    global username
-    global password
-    global error
-
-    username = ""
-    password = ""
-    if error:
+    session["username"] = ""
+    session["password"] = ""
+    if session["error"]:
         return render_template("signin.html", error=True)
     return render_template("signin.html", error=False)
 
+
 @app.route("/marks")
 def marks():
-    global error
-
     scrape = MaplewoodScraper(username, password)
     if scrape.start(notify=False):
-        error = False
+        session["error"] = False
         return render_template("index.html", courses=scrape.courses, aliases=scrape.aliases)
-    error = True
+    session["error"] = True
     return redirect("/signin")
+
 
 if __name__ == "__main__":
     app.run()
